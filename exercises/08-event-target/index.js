82d94770f89ba78e75d67c38c5a1d8985b7c8524
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-04-28 21:27:54 +0000

    Pending changes exported from your codespace

--- exercises/08-event-target/index.js
+++ exercises/08-event-target/index.js
@@ -1,6 +1,14 @@
 window.onload = function loadFn() {
-	var containerElm = document.getElementById("container");
-	containerElm.addEventListener("click", function(event) {
-		//you code here
+	var btn1 = document.getElementById("btn1");
+	var anchor1 = document.getElementById("anchor1");
+	var img1 = document.getElementById("img1");
+	btn1.addEventListener("click", function(event) {
+		alert(`Se hizo clic en ${event.target.id}`);
+	});
+	anchor1.addEventListener("click", function(event) {
+		alert(`Se hizo clic en ${event.target.id}`);
+	});
+	img1.addEventListener("click", function(event) {
+		alert(`Se hizo clic en ${event.target.id}`);
 	});
 };
